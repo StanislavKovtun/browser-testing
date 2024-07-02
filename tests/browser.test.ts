e248814670971f63ad2801874c2a4e0ec297c025
Stanislav Kovtun <sstanisslaw@gmail.com>
2024-07-02 19:26:27 +0300

    Test iPhone11

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -1,4 +1,20 @@
-import { test } from "@playwright/test";
+// import { test } from "@playwright/test";
+
+// test("test browser", async ({ page }) => {
+//   // point this to wherever you want
+//   // await page.goto("http://localhost:3000/");
+//   await page.goto("http://localhost:5000/");
+
+//   // keep browser open
+//   await page.pause();
+// });
+
+import { test, devices } from "@playwright/test";
+
+// Define the device you want to emulate
+const iPhone11 = devices["iPhone 11"];
+
+test.use({ ...iPhone11 });
 
 test("test browser", async ({ page }) => {
   // point this to wherever you want
